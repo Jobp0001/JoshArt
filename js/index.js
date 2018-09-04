4970b696761a348a37c6d75a5a76253639e74efe
Joseph Job <35570114+Jobp0001@users.noreply.github.com>
2018-09-04 16:07:05 -0400

    Added Animation for Skill Boxes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,38 @@
 $(document).ready(function(){
+    const skills = $('.skill');
+
     console.log("works");
     const controller = new ScrollMagic.Controller();
-    const ourScene = new ScrollMagic.Scene({
+    const aboutScene = new ScrollMagic.Scene({
         triggerElement: '#aboutMe',
         triggerHook: 0.7,
+        reverse: false
     });
-    ourScene.setClassToggle('#aboutMe', 'fade-in');
-    ourScene.addIndicators({
-        name: 'fade scane',
+    aboutScene.setClassToggle('#aboutMe', 'fade-in');
+    aboutScene.addIndicators({
+        name: 'fade scene',
         colorTrigger: 'black',
         colorStart: '#75c695'
     });
-    ourScene.addTo(controller);
+    aboutScene.addTo(controller);
+
+    const skillTimeline = new TimelineMax();
+    skillTimeline
+        .staggerFrom(skills, 0.8, {y: 150, autoAlpha: 0, ease: Power3.easeOut}, 0.1);
+
+
+    const skillScene = new ScrollMagic.Scene({
+        triggerElement: '#skillContainer',
+        triggerHook: 0.4,
+        reverse: false
+    })
+        .setTween(skillTimeline)
+        .addIndicators({
+            name: 'fade skill',
+            colorTrigger: "red",
+            colorStart: "#4582cc"
+        })
+        .addTo(controller);
+
 });
+
